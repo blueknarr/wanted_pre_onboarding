6779da6c53bf57d8553a3d8340b7f9ae1a1384fc
Yong Su Jeong <knarrblue@gmail.com>
2022-06-19 18:19:27 +0900

    update api 추가

--- app/views/api.py
+++ app/views/api.py
@@ -39,3 +39,36 @@ def register():
     except SQLAlchemyError as sqlerr:
         db.session.rollback()
         return jsonify({'result': 'failed', 'msg': sqlerr})
+
+
+@bp.route('/update', methods=['POST'])
+def update():
+    """
+    채용 공고 내용을 수정하는 api
+    채용 공고 id로 해당하는 내용을 찾고
+    채용 직무, 보너스, 직무 설명서, 기술 스택 변경 내용을 수정
+    :return: dict
+    """
+    id = request.form['id']
+    job_position = request.form['job_position']
+    bonus = request.form['bonus']
+    job_description = request.form['job_description']
+    tech_stack = request.form['tech_stack']
+
+    job_posting = JobPosting.query.get(id)
+
+    if job_posting:
+        try:
+            job_posting.job_position = job_position
+            job_posting.bonus = bonus
+            job_posting.job_description = job_description
+            job_posting.tech_stack = tech_stack
+
+            db.session.add(job_posting)
+            db.session.commit()
+            return {'result': 'success'}
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            return jsonify({'result': 'failed', 'msg': e})
+    else:
+        return {'result': 'failed', 'msg': f'{id}번 채용 공고가 없습니다.'}
